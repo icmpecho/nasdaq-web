11bdcabbe27c8cea6e2719ee4dada166eb1901ed
IcmpEcho <pinnapong@gmail.com>
2014-03-10 00:21:54 +0700

    Add format_data_for_morris method helper

--- app/scripts/helper.js
+++ app/scripts/helper.js
@@ -3,6 +3,16 @@ function format_time (unix_epoch) {
 	return dt.getHours() + ':' + dt.getMinutes();
 }
 
+function format_data_for_morris (data) {
+	var ret = [];
+	var i;
+	for(i = 0; i < data.length; i++) {
+		ret.push({time: format_time(data[i]._ts), index: data[i].index});
+	}
+	return ret;
+}
+
 if (typeof module !== 'undefined' && module.exports != null) {
     exports.format_time = format_time;
+    exports.format_data_for_morris = format_data_for_morris;
 }
--- test/spec/test.js
+++ test/spec/test.js
@@ -10,5 +10,18 @@
 					helper.format_time(1394380727).should.equal('22:58');
         });
       });
+      describe('format_data_for_morris', function () {
+      	it('should return data formatted for morris', function () {
+      		var data = [
+      			{ _ts: 1394348327, index: 4336.22, change: -15.91 },
+      			{ _ts: 1394351935, index: 4346.22, change: -5.91 }
+      		];
+      		var expected = [
+      			{ time: "13:58", index: 4336.22 },
+      			{ time: "14:58", index: 4346.22 }
+      		];
+      		helper.format_data_for_morris(data).should.eql(expected);
+      	});
+      });
     });
 })();
