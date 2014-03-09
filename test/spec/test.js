bb37165c1fb67779d531f0b5b61d83beccfd7d49
IcmpEcho <pinnapong@gmail.com>
2014-03-09 23:56:02 +0700

    Fix indention

--- test/spec/test.js
+++ test/spec/test.js
@@ -5,10 +5,10 @@
     var should = require('should');
     var helper = require('../../app/scripts/helper.js');
     describe('helper.js', function () {
-        describe('format_time', function () {
-            it('should return correct hour and minute from unix epoch', function () {
-							helper.format_time(1394380727).should.equal('22:58');
-            });
+      describe('format_time', function () {
+        it('should return correct hour and minute from unix epoch', function () {
+					helper.format_time(1394380727).should.equal('22:58');
         });
+      });
     });
 })();
